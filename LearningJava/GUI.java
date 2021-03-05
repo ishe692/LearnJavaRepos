3bdc96e1af5d21722d91949d14f68d0a490143ae
acer <acer@192.168.1.74>
2021-03-05 15:56:57 +1300

    explosion button now opperational

--- LearningJava/GUI.java
+++ LearningJava/GUI.java
@@ -1,17 +1,36 @@
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class GUI {
+public class GUI implements ActionListener{
+	
+	
+	private int count = 0;
+	
+	private JLabel label;
+	private JFrame frame;
+	private JPanel panel;
 	
 	public GUI() {
-		JFrame frame = new JFrame();
-		JPanel panel = new JPanel();
+		
+		JButton button = new JButton("Exlosion");
+		label = new JLabel("Number of explosions");
+		frame = new JFrame();
+		panel = new JPanel();
+		
+		button.addActionListener(this);
+		
 		panel.setBorder(BorderFactory.createEmptyBorder(30,30,10,30));
 		panel.setLayout(new GridLayout(0,1));
+		panel.add(button);
+		panel.add(label);
 		
 		frame.add(panel,BorderLayout.CENTER);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -21,9 +40,20 @@ public class GUI {
 		
 	}
 	
+	
 	public static void main(String[] args) {
 		
 		new GUI();
+		
+	}
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		count++;
+		label.setText("Number of explosions: " + count);
+		
 		
 	}
 
